ccf29f309acd2a38d187ebdc31a84b14a5fb93aa
Coleman <penguincoder@gmail.com>
2008-10-11 17:40:01 -0500

    adding favorite support, enhancing the photo browser, use the photo browser for favorites, allow voting controls on any page that uses the photo partial

--- app/controllers/favorites.rb
+++ app/controllers/favorites.rb
@@ -1,17 +1,21 @@
 class Favorites < Application
   before :logged_in?
-  before :fetch_allowed_user, :only => [ :show ]
-  only_provides :xml
   
   def show
-    only_provides :html
-    @photos = @user.favorite_photos
-    render
+    @page = params[:page].to_i
+    per_page = 24
+    @page_count = (current_user.photo_favorites.count(:id).to_f / per_page.to_f).ceil
+    @photos = current_user.favorite_photos.find :all, :order => 'id DESC', :limit => per_page, :offset => (per_page * @page)
+    if request.xhr?
+      partial 'photos/photo_browser'
+    else
+      render
+    end
   end
   
   def create
-    raise NotAllowed unless request.xhr?
-    @photo = Photo.find params[:id]
+    raise NotAcceptable unless request.xhr?
+    @photo = Photo.find params[:photo_id] rescue nil
     pf = PhotoFavorite.new :photo_id => @photo.id, :user_id => current_user.id
     if pf.save
       render '', :status => 200
@@ -20,10 +24,12 @@ class Favorites < Application
     end
   end
   
-  def delete
-    raise NotAllowed unless request.xhr?
-    pf = PhotoFavorite.find params[:id], :include => :user
-    if pf.user == current_user and pf.destroy
+  def destroy
+    raise NotAcceptable unless request.xhr?
+    pf = current_user.photo_favorites.detect do |f|
+      f.photo_id == params[:id].to_i
+    end
+    if pf and pf.user_id == current_user.id and pf.destroy
       render '', :status => 200
     else
       render '', :status => 403
