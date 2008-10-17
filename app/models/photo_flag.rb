44f525adbbe9538248120a547d6b367ae75d4485
Coleman <penguincoder@gmail.com>
2008-10-17 01:34:06 -0500

    use the right scope

--- app/models/photo_flag.rb
+++ app/models/photo_flag.rb
@@ -1,5 +1,5 @@
 class PhotoFlag < ActiveRecord::Base
   belongs_to :photo, :counter_cache => true
   belongs_to :user
-  validates_uniqueness_of :user_id
+  validates_uniqueness_of :user_id, :scope => :photo_id
 end
