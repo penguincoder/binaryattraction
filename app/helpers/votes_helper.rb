a66821f0e7574489b94041c305827cf472a7243e
Coleman <penguincoder@gmail.com>
2008-10-17 02:36:15 -0500

    use bigger image for stats and remove text

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -2,9 +2,9 @@ module Merb
   module VotesHelper
     def stat_chart
       curl = Gchart.pie(
-        :size => '300x200',
-        :title => "Voting results for #{@user.votes.size} photos",
-        :legend => [ 'Not One', 'One' ],
+        :size => '415x275',
+        :title => "Voting results for #{@user.votes.size} photos. Oneness #{"%.1f%%" % (@user.votes.select { |v| v.one? }.size.to_f / @user.votes.size.to_f * 100.0)}",
+        :legend => [ "Not One (#{@user.votes.select { |v| v.zero? }.size})", "One (#{@user.votes.select { |v| v.one? }.size})" ],
         :data => [ @user.votes.select { |v| v.zero? }.size, @user.votes.select { |v| v.one? }.size ],
         :theme => :pastel
       )
