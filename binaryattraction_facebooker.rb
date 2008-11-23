607589db21cb3d6bdcc247cdfe07303a043d6bb3
Coleman <penguincoder@gmail.com>
2008-11-23 15:11:12 -0600

    need fixed width

--- binaryattraction_facebooker.rb
+++ binaryattraction_facebooker.rb
@@ -264,7 +264,7 @@ __END__
       overflow: hidden
       padding: 0
       position: relative
-      width: 100%
+      width: 644px
       .table_wrapper
         padding: 0 5px
         margin: 0
